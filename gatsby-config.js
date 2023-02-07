c4d849757d0b7f0ba3577da9b83e3e869e151dc1
Aleksi O'Connor <aleksi@sharetribe.com>
2023-02-07 16:52:56 +0200

    test path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,10 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+
 module.exports = {
+  pathPrefix: "/docs",
   siteMetadata: {
     title: `Gatsby + Vercel`,
     siteUrl: `https://gatsby-template.vercel.app/`,
